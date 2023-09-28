97637763ba63fe05cf44472de5f1107297ff6452
Lei Wang <leiwang.lsu@gmail.com>
2023-09-27 21:37:46 -0500

    make it work

--- KKFOne/FormMain.vb
+++ KKFOne/FormMain.vb
@@ -272,6 +272,10 @@ Public Class FormMain
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
+            If (H Is Nothing) Then
+                MsgBox("H is not defined")
+                Return
+            End If
             TextBoxOutput.Text = ""
             For i As Integer = 0 To H.Length - 1
                 PrintMatrix(H(i), TextBoxOutput.Text)
